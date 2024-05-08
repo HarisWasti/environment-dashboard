39cca4773840050aa683e32a21fb5adb4c388610
HarisWasti <152641801+HarisWasti@users.noreply.github.com>
2024-05-08 09:49:49 +0100

    Update first_app.py

--- Data_science_cw_2/first_app.py
+++ Data_science_cw_2/first_app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Load data
-data = pd.read_csv("environment.csv")
+data = pd.read_csv("https://github.com/HarisWasti/environment-dashboard/blob/main/Data_science_cw_2/enviroment.csv")
 
 # Set the title of the Streamlit app
 st.title("Environmental Damage")
